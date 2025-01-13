17980802b4c35312edd0189c57327235b68159a4
Matthias Seeger <matthis@amazon.de>
2025-01-13 12:01:39 +0100

    Moved new code to separate repo

--- litgpt/kvcache/__init__.py
+++ litgpt/kvcache/__init__.py
@@ -4,20 +4,10 @@ from litgpt.kvcache.base import (
     KVCacheParams,
     MostRecentKVCache,
 )
-from litgpt.kvcache.attn_weights import AttnWeightsKVCache
-from litgpt.kvcache.h2o import (
-    H2OKVCache,
-    H2OOriginalKVCache,
-    VLengthH2OKVCache,
-)
 
 __all__ = [
-    "AttnWeightsKVCache",
     "DenseKVCache",
-    "H2OKVCache",
-    "H2OOriginalKVCache",
     "KVCache",
     "KVCacheParams",
     "MostRecentKVCache",
-    "VLengthH2OKVCache",
 ]
